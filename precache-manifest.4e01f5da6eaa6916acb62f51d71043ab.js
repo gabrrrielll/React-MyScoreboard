614d74ce8d5f1725efe722de65e97cd1b87b8068
Gabriel Sandu <allmediacreation@gmail.com>
2019-04-04 21:59:51 +0300

    Updates

---
+++ precache-manifest.4e01f5da6eaa6916acb62f51d71043ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f03ba8f8bc3e33013603",
+    "url": "/MyScoreboard/static/js/runtime~main.d9173a1c.js"
+  },
+  {
+    "revision": "a3390c2c68408d72006f",
+    "url": "/MyScoreboard/static/js/main.9e7624b3.chunk.js"
+  },
+  {
+    "revision": "bb942f725e7db3df76af",
+    "url": "/MyScoreboard/static/js/2.1fe07213.chunk.js"
+  },
+  {
+    "revision": "a3390c2c68408d72006f",
+    "url": "/MyScoreboard/static/css/main.eb41a183.chunk.css"
+  },
+  {
+    "revision": "e94214333b62be2fce62df10eab4b972",
+    "url": "/MyScoreboard/index.html"
+  }
+];
